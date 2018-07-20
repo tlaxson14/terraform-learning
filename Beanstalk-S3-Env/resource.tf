4e9574487c90e26bd779aedeaa81395dfefe78c3
Travis Laxson <laxsont@oregonstate.edu>
2018-07-20 00:57:54 -0500

    initial commit - S3 bucket + bucket objects

---
+++ Beanstalk-S3-Env/resource.tf
@@ -0,0 +1,32 @@
+#Create S3 Bucket
+resource "aws_s3_bucket" "myS3Bucket" {
+  bucket = "${var.s3BucketName}"
+  acl    = "public-read"
+
+  tags {
+    Name        = "My Terraform Dev-Challenge Bucket"
+    Environment = "Dev"
+  }
+}
+
+#Upload index.html file to S3 bucket
+resource "aws_s3_bucket_object" "indexObject" {
+  bucket       = "${var.s3BucketName}"
+  key          = "index.html"
+  source       = "/Users/TravisLaxson/Terraform/code/terraform-learning/Beanstalk-S3-Env/index.html"
+  acl          = "public-read"
+  content_type = "text/html"
+  etag         = "${md5(file("/Users/TravisLaxson/Terraform/code/terraform-learning/Beanstalk-S3-Env/index.html"))}"
+  depends_on   = ["aws_s3_bucket.myS3Bucket"]
+}
+
+#Upload error.html file to S3 bucket
+resource "aws_s3_bucket_object" "errorObject" {
+  bucket       = "${var.s3BucketName}"
+  key          = "error.html"
+  source       = "/Users/TravisLaxson/Terraform/code/terraform-learning/Beanstalk-S3-Env/error.html"
+  acl          = "public-read"
+  content_type = "text/html"
+  etag         = "${md5(file("/Users/TravisLaxson/Terraform/code/terraform-learning/Beanstalk-S3-Env/error.html"))}"
+  depends_on   = ["aws_s3_bucket.myS3Bucket"]
+}
